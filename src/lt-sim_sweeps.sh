7a95a2f42bb8abe44601452f50c283389bec328b
Ashley Mae Conard <ashleymaeconard@gmail.com>
2023-01-26 23:55:10 -0500

    updated comments

--- src/lt-sim_sweeps.sh
+++ src/lt-sim_sweeps.sh
@@ -1,23 +1,21 @@
 # lt-sim_sweeps.sh
-# Ashley Conard
-# Last Mod. 11/17/21
 
 # Check to make sure input is correct
 if [ $# -le 12 ]; then
 	echo 'Usage: ./lt-sim_sweeps.sh 
             1) seed                  (integer) 
-            2) /PATH/TO/mask         (path to mask or type NA) 
-            3) /PATH/TO/output_dir   (path for generated output)
-            4) degree                (0 or positive integer for degree)
-            5) ind                   (number of individuals in the population)
-            6) k                     (prevalence of the disease/trait in population)
-            7) pve                   (broad sense heritability)
-            8) fst                   (targeted fixation index as a measure of population \\ differentiation due to genetic structure (suggest 0.05))
-            9) obs                   (total observed population)
-            10) prop_case            (proportion of case observations) 
-            11) tot_snp_sim          (total number SNPs in population)
-            12) frac_causal          (proportion of causal SNPs in population)
-            13) hierarchy            (sample causal SNPs according to biological annotations (e.g., genes or pathways)
+            2) /PATH/TO/output_dir   (path for generated output)
+            3) /PATH/TO/ind          (FILE, number of individuals in the population)
+            4) /PATH/TO/k            (FILE, prevalence of the disease/trait in population)
+            5) /PATH/TO/pve          (FILE, broad sense heritability)
+            6) /PATH/TO/fst          (FILE, targeted fixation index as a measure of population \\ differentiation due to genetic structure (suggest 0.05))
+            7) /PATH/TO/obs          (FILE, total observed population)
+            8) /PATH/TO/prop_case    (FILE, proportion of case observations) 
+            9) /PATH/TO/tot_snp_sim  (FILE, total number SNPs in population)
+            10) /PATH/TO/frac_causal (FILE, proportion of causal SNPs in population)
+            11) hierarchy            (0:no or 1:yes, sample causal SNPs according to biological annotations (e.g., genes or pathways)
+            12) /PATH/TO/mask        (NA:no, or path to mask) 
+            13) degree               (0:no or positive integer for degree of each gene connections to SNP(s))
          '
 	exit 1
 fi
@@ -29,15 +27,20 @@ Generating simulations for 10 replicates per parameter set.
 
 # Primary parameters
 SEED=$1
-MASK_DIR=$2
-SIM_OUTDIR=$3
-RES_OUTDIR=$4
-NUM_PROCESSORS=$5
-NON_OVERLAP_USED=$6
-K_FILE=$7
-OBS_FILE=$8
-PVE_FILE=$9
+SIM_OUTDIR=$2
 
+IND_FILE=$3
+K_FILE=$4
+PVE_FILE=$5
+FST_FILE=$6
+OBS_FILE=$7
+PROP_CASE=$8
+TOT_SNP_SIM=$9
+FRAC_CAUSAL=$10
+
+HIER=$11
+MASK_DIR=$12
+DEG=$13
 
 echo "Number processors: $NUM_PROCESSORS"
 echo "K, observations, PVE: $K_FILE, $OBS_FILE, $PVE_FILE"
@@ -63,24 +66,39 @@ START=0
 i=$START
 
 # Create grid of parameter search
-for k in $(awk '{print $k}' $K_FILE)
+for ind in $(awk '{print $ind}' $IND_FILE)
 do  
-    for obs in $(awk '{print $obs}' $OBS_FILE)
+    for k in $(awk '{print $k}' $K_FILE)
     do  
-        for pve in $(awk '{print $pve}' $PVE_FILE);  
-        do
-            echo "Combination: seed $SEED, k $k, samples $obs, and PVE (H^2) $pve"
-            ((i = i + 1)) # Incrementing counter
-            #((SEED = SEED + 1)) # Incrementing seed
-            # If number of processors reached, add wait to form a batch that will finish, and then process the next batch
-            if (( ${i}%${NUM_PROCESSORS}==0 )); then # was 
-                echo "wait" >> $COMMAND_SCRIPT
-            fi 
+        for pve in $(awk '{print $pve}' $PVE_FILE)
+        do  
+            for fst in $(awk '{print $fst}' $FST_FILE);  
+            do
+                for obs in $(awk '{print $obs}' $OBS_FILE);  
+                do
+                    for prop_case in $(awk '{print $prop_case}' $PROP_CASE_FILE);  
+                    do
+                        for tot_snp_sim in $(awk '{print $tot_snp_sim}' $TOT_SNP_SIM_FILE);  
+                        do
+                            for frac_causal in $(awk '{print $frac_causal}' $FRAC_CAUSAL_FILE);  
+                            do
+                                echo "Combination: seed $SEED, k $k, samples $obs, and PVE (H^2) $pve"
+                                ((i = i + 1)) # Incrementing counter
+                                
+                                # If number of processors reached, add wait to form a batch that will finish, and then process the next batch
+                                if (( ${i}%${NUM_PROCESSORS}==0 )); then # was 
+                                    echo "wait" >> $COMMAND_SCRIPT
+                                fi 
 
-            # conda activate multio_case_ctrl
-            echo "( /usr/bin/time Rscript lt-sim.r $SEED $MASK_DIR $SIM_OUTDIR $RES_OUTDIR $NON_OVERLAP_USED $k $obs $pve > $RES_OUTDIR/run_out.seed$SEED-k$k-obs$obs-pve$pve.txt 2>&1) &">> $COMMAND_SCRIPT
-        done
-    done  
+                                # conda activate multio_case_ctrl
+                                echo "( /usr/bin/time Rscript lt-sim.r $SEED $MASK_DIR $SIM_OUTDIR $NON_OVERLAP_USED $k $obs $pve > $RES_OUTDIR/run_out.seed$SEED-k$k-obs$obs-pve$pve.txt 2>&1) &">> $COMMAND_SCRIPT
+                            done
+                        done
+                    done
+                done
+            done
+        done  
+    done
 done 
 
 # Run command
